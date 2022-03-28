1b9a28e690ab334edce05bb941a1b510cff9bbda
Laura Geeding <laurageeding@Lauras-MacBook-Pro.local>
2022-03-28 13:56:06 -0500

    Exercise3

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -2,6 +2,7 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class SchoolPractice {
     public static void main(String[] args) {
-        // Instantiate your Student class for part 2 here!
+        Student student1 = new Student("Laura", 24601, 1, 4.0);
+        System.out.println(student1.getName());
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -7,7 +7,28 @@ public class Student {
 
     private String name;
     private int studentId;
-    private int numberOfCredits = 0;
-    private double gpa = 0.0;
+    private int numberOfCredits;
+    private double gpa;
 
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String aName) {
+        name = aName;
+    }
+
+    public int getId() {
+        return studentId;
+    }
+    protected void setId(int aId) {
+        studentId = aId;
+    }
+    
 }
